cf05d1e2cb3c5e5f8218cb70bf58e6964163fbf5
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-02 21:09:42 +0900

    Update upNext-video , CSS btn-color

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ moreBtn.addEventListener('click',() => {
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
 });
+
+
+// UpNext hover Play
+
+const upNextVideo = document.querySelector('.upNext');
+
+upNextVideo.addEventListener('mouseover', (e) => {
+    const videoId = e.target;
+    if (videoId.id === 'video') { videoId.setAttribute('controls', 'controls'); }
+});
+
+upNextVideo.addEventListener('mouseout', (e) => {
+    const videoId = e.target;
+    if (videoId.id === 'video') { videoId.removeAttribute('controls', 'controls'); }
+});
+
+
